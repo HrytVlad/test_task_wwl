3fcb7f287cb4724eed3b1f6d6ba1785f5aaf9322
HrytVlad <sunrises2929@gmail.com>
2023-08-29 16:04:59 +0300

    Created models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    probation = models.BooleanField(default=False)
+    position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.user.first_name} - {self.position}'
+
+
+class Order(models.Model):
+    task_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} ({self.task_id}) by {self.employee.user.first_name}'
