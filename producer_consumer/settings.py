b31abd04f411c640a8c886492ea9dfe152a2776c
HrytVlad <sunrises2929@gmail.com>
2023-08-29 16:05:32 +0300

    Connect postgresql

--- producer_consumer/settings.py
+++ producer_consumer/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'producer_consumer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wwl',
+        'USER': 'postgres',
+        'PASSWORD': 'dkfl1998',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
